5a12658e0beec0026a303dab0d3271ae78ec3a69
Jiachun Xiang <max997123@gmail.com>
2023-04-17 20:47:31 -0400

    initialize dynamic html for speakers

--- popup.js
+++ popup.js
@@ -1,92 +1,74 @@
 const data = [{
-    Name: 'Neha Kapoor',
-    title: 'Professor, Computer Science at Melboure Information University',
+    name: 'Neha Kapoor',
+    title: 'Professor at Melboure Information University',
     description: `Head researcher for QNN, Quantum Neutral Network, an 
     exploration of the relationship between Quantum Computing and Neutral Network.`,
-    image: './img/p1.png',
+    image: './img/p1.jpeg',
   },
   
   {
-    Name: 'Grace Vanderbilt',
+    name: 'Grace Vanderbilt',
     title: 'Founder, WinS at Melboure Information University',
     description: `Inspiring young women to have their journey not also
     in science, but also in other STEM field.`,
-    image: './img/p2.png',
+    image: './img/p2.jpeg',
   },
   
   {
-    title: 'Multi Post Stories',
-    technologies: '<li>HTML</li><li>Bootstrap</li><li>Ruby</li>',
-    description: `A daily selection of privately personalized reads;
-    no accounts or sign-ups required. has been the
-    industry's standard.`,
-    liveDemoLink: 'https://example.com',
-    sourceCodeLink: 'https://github.com/example',
-    image: './img/work.png',
+    name: 'Aaliyah Williams',
+    title: 'Director, BSS at Melboure Information University',
+    description: `Vice president at Melboure Information University, 
+    biochemical professor with research field of protein structure.`,
+    image: './img/p3.jpeg',
   },
   
   {
-    title: 'Multi Post Stories',
-    technologies: '<li>HTML</li><li>Bootstrap</li><li>Ruby</li>',
-    description: `A daily selection of privately personalized reads;
-    no accounts or sign-ups required. has been the
-    industry's standard.`,
-    liveDemoLink: 'https://example.com',
-    sourceCodeLink: 'https://github.com/example',
-    image: './img/work.png',
+    name: 'Nathan Adams',
+    title: 'CEO, Human Immnune Porject, Harvard.M.C',
+    description: `One of the founder for HIP, which helps people to 
+    reduce the chances of getting cancers for people with their 
+    exploration on biocomputing.`,
+    image: './img/p4.jpeg',
   },
   
   {
-    title: 'Multi Post Stories',
-    technologies: '<li>HTML</li><li>Bootstrap</li><li>Ruby</li>',
-    description: `A daily selection of privately personalized reads;
-    no accounts or sign-ups required. has been the
-    industry's standard.`,
-    liveDemoLink: 'https://example.com',
-    sourceCodeLink: 'https://github.com/example',
-    image: './img/work.png',
+    name: 'Jasmine Ng',
+    title: 'Director, TrAnsMe in New York',
+    description: `PhD at University College London, now work in 
+    NASA, founder of TrAnsMe to promote transgender identity for
+    Asian.`,
+    image: './img/p5.jpeg',
   },
   
   {
-    title: 'Multi Post Stories',
-    technologies: '<li>HTML</li><li>Bootstrap</li><li>Ruby</li>',
-    description: `A daily selection of privately personalized reads;
-    no accounts or sign-ups required. has been the
-    industry's standard.`,
-    liveDemoLink: 'https://example.com',
-    sourceCodeLink: 'https://github.com/example',
-    image: './img/work.png',
+    name: 'Jean Fitzgerald',
+    title: 'Head Manager, ODiSsey',
+    description: `She uses interesting and attractive ways to 
+    explore the science world for kids. Let inpirations around 
+    children's mind and have motivation to learn more.`,
+    image: './img/p6.jpeg',
   },
-  
-  {
-    title: 'Multi Post Stories',
-    technologies: '<li>HTML</li><li>Bootstrap</li><li>Ruby</li>',
-    description: `A daily selection of privately personalized reads;
-    no accounts or sign-ups required. has been the
-    industry's standard.`,
-    liveDemoLink: 'https://example.com',
-    sourceCodeLink: 'https://github.com/example',
-    image: './img/work.png',
-  },
+
   ];
   const normalDiv = document.getElementById('div-container');
-  for (let j = 1; j < data.length; j += 1) {
+  for (let j = 0; j < data.length; j += 1) {
     normalDiv.innerHTML += `
-    <div class="normal">
+    <div id="top-work">
+    <div class="img-container">
       <img src="${data[j].image}" alt="Art Selection Project Image" />
-      <div class="div-text">
-          <h1 aria-label="Profesional Art Printing Data" class="hover">${data[j].title}</h1>
-          <p aria-label="A daily selection of privately personalized reads;
+      </div>
+      <div class="text-container">
+          <h1 aria-label="Profesional Art Printing Data"">${data[j].name}</h1>
+          <h2 aria-label="A daily selection of privately personalized reads;
           no accounts or sign-ups required. has been the
           industry's standard" class="hover">
+          ${data[j].title}
+          
+          </h2>
+          <p>
           ${data[j].description}
           </p>
-          <ul>
-          ${data[j].technologies}
-          </ul>
-  
-      <button type="button" class="hover-btn popup-trigger" aria-label="press to see the project" id="${data[j].id}">See project</button>
-    </div>
+      </div>
     </div>
         `;
   }
